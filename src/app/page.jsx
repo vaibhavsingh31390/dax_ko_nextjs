f924a847e99599e12ecb8bbaab5a1be8142a015e
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2025-02-05 16:07:52 +0530

    [INIT]

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,18 @@
+import Button from "@/components/common/forms/Button";
 import { Col, Container, Row } from "@/components/layout/Format";
 
 export default function Home() {
   return (
-    <Container className="mt-[200px]">
+    <Container>
       <Row>
-        <Col sm={12} md={12} lg={12} className="min-h-[70svh]">
+        <Col sm={12} md={12} lg={12} className="py-[50px] md:py-[100px]">
           <h1>Spider-Man’s 60th</h1>
-          <h2>
+          <h2 className="mt-[9px] md:mt-[18px]">
             Celebrate Spidys 60th annivessary with most of is most iconic
             stories!
           </h2>
+
+          <Button className={`mt-[30px] md:mt-[60px]`} value={`Read More`} />
         </Col>
       </Row>
     </Container>
