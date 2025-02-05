779e36b1db999353141618a993d7ac933213aa69
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2025-02-06 00:27:54 +0530

    [UPDATE] changes

--- src/app/(home_sections)/HeroSection.jsx
+++ src/app/(home_sections)/HeroSection.jsx
@@ -8,8 +8,8 @@ function HeroSection() {
         <Col sm={12} md={12} lg={12} className="py-[50px] md:py-[100px]">
           <h1>Spider-Man’s 60th</h1>
           <h2 className="mt-[9px] md:mt-[18px]">
-            Celebrate Spidys 60th annivessary with most of is most iconic
-            stories!
+            Celebrate Spidys 60th annivessary with most of is most
+            <br /> iconic stories!
           </h2>
           <Button className={`mt-[30px] md:mt-[60px]`} value={`Read More`} />
         </Col>
