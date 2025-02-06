3136dbc2921898b5a597d45078901a7f2419922d
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2025-02-06 17:06:18 +0530

    [UPDATE] fixes

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -11,7 +11,6 @@ import useFetch from "@/hooks/useFetch";
 
 function Modal({ url, options, handleModal }) {
   const { data, loading, error, fetchData } = useFetch(url, options);
-  console.log(url);
 
   // const toastOption = {
   //   duration: 3000,
